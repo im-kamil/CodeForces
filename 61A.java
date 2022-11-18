5061d0a5c0c2a94f7d0e114159879a53abfd2da5
im-kamil <kamilnasar786@gmail.com>
2022-11-19 00:52:28 +0530

    ultra fast mathematician

---
+++ 61A.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+
+public class 61A {
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+
+        String a=sc.nextLine();
+        String b=sc.nextLine();
+
+        char[] c=new char[a.length()];
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)=='0'){
+                if(b.charAt(i)=='1'){
+                    c[i]='1';
+                }
+                else{
+                    c[i]='0';
+                }
+            }
+            else{
+                if(b.charAt(i)=='0'){
+                    c[i]='1';
+                }
+                else{
+                    c[i]='0';
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
