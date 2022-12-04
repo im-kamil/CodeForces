ea3884257f0d4db732cde2edbcb672a7c1d5a7b3
im-kamil <kamilnasar786@gmail.com>
2022-12-04 17:42:58 +0530

    chala jaa on github

---
+++ hellojava.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class hellojava {
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       int t = sc.nextInt();
+       while(t-->0){
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(a>=b){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+       }
+    }
+}
