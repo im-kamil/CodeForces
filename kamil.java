d17b4af44cbe537ea520a959b1547a5e42a88ad2
im-kamil <kamilnasar786@gmail.com>
2022-12-18 14:32:48 +0530

    a .java

---
+++ kamil.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class kamil {
+    public static void main(String[] args){
+        int a = 10;
+        Scanner sc = new Scanner(System.in);
+         a = sc.nextInt();
+         System.out.println(a);
+    }
+}
